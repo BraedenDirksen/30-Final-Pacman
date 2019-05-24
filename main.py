99e6658feefb6ddc3085c6a77abe4fb89972f55c
Braeden Dirksen <b.dirske@share.epsb.ca>
2019-05-24 11:45:13 -0600

    did things

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ author: micheal zhang
 date: 2019-04-08
 '''
 import pygame
+from myClass import *
 pygame.init() # loads the pygame module comands in the program
 
 # display variables
 TITLE = "hello world" # title that appears in the window title
 FPS = 30
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 232
+HEIGHT = 256
 SCREENDIM = (WIDTH, HEIGHT)
 
 # color variables
@@ -22,19 +23,24 @@ GREY = (50,50,50)
 screen = pygame.display.set_mode(SCREENDIM)
 # creates main surface where all other assets are placed on top
 pygame.display.set_caption(TITLE) # updates window title to "TITLE"
-screen.fill(GREY) # fills the entire surface with "GREY"
+screen.fill(BLACK) # fills the entire surface with "GREY"
 clock = pygame.time.Clock() # starts a clock object to measure time
 
 
 #--- code starts here ---#
 
 
-pacman = player()
+#pacman = player()
+background = mySprite("shtuff/maze.png")
+
+
 running = True
 while running:
     for event in pygame.event.get(): # returns all inputs amd triggers into an array
         if event.type == pygame.QUIT:
             running = False
+
+    screen.blit(background.surface,background.getPos())
 
     clock.tick(FPS) # pause the game until the FPS time is reached
     pygame.display.flip() # update the screen with changes
--- myClass.py
+++ myClass.py
@@ -5,7 +5,8 @@ date created 2019-04-15
 '''
 import pygame
 class myClass:
-    def __init__(self,color,x = 0,y = 0):
+    import pygame
+    def __init__(self,color = (0,0,0),x = 0,y = 0):
         self.surface = pygame.Surface((0,0),pygame.SRCALPHA)
         self.red = 0
         self.green = 0
@@ -25,6 +26,12 @@ class mySprite(myClass):
     def __init__(self,fileName):
         myClass.__init__(self)
         self.surface = self.pygame.image.load(fileName).convert_alpha()
+        self.mask = pygame.mask.from_surface(self.surface)
+        self.x = 0
+        self.y = 0
+        self.pos = (self.x,self.y)
+    def getPos(self):
+        return self.pos
 
 class player(myClass):
     def __init__(self,height,width,x=0,y=0,color = (0,0,0),dirx=1,diry=1):
@@ -90,7 +97,15 @@ def ghost(myClass):
     def getY(self):
         return self.y
     def move(self,WIDTH,HEIGHT, spdX = 20, spdY = 20):
-        
+        pass
+
+def getCollisionPoint(sprite1,sprite2):
+    offset = (sprite2.getPos()[0] - sprite1.getPos()[0],sprite2.getPos()[1] - sprite1.getPos()[1]
+    collisionPoint = sprite1.mask.overlap(sprite2.mask,offset)
+    if collisionPoint:
+        return True
+    else:
+        return False
 
 def getSpriteCollision(sprite1, sprite2):
     if sprite2.getX() <= sprite1.getX() + sprite1.getWidth() <= sprite2.getX() + sprite2.getWidth() + sprite1.getWidth() and sprite2.getY() <= sprite1.getY() + sprite1.getHeight() <= sprite2.getY() + sprite2.getHeight() + sprite1.getHeight():
